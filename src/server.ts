a6100b247e2e1406be0bf9ff9c82be6f536d9ee9
ramonFrancisco <ramon.francisco.silva99@gmail.com>
2020-04-14 21:46:48 -0300

    Initial commit

---
+++ src/server.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+import routes from './routes';
+
+const PORT = 3333;
+
+const app = express();
+
+app.use(routes);
+
+app.listen(PORT, () => {
+  console.log(`Application started in port: ${PORT}`);
+});
